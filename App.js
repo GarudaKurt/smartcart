c2bf081c810835189e4dbff91fb2e110892f817c
garudakurm <garudakurt@gmail.com>
2024-03-29 20:19:22 -1200

    progress

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+import SignIn from './components/SignIn';
+import SignUp from './components/SignUp';
+import Cashier  from './components/Cashier';
+import Admin from './components/Admin';
+import Customer from './components/Customer';
+import QRCodeDisplay from './components/QRCodeDisplay';
+
+const Stack = createStackNavigator();
+
+const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name='Login' component={SignIn} options={{headerShown:false}}/>
+        <Stack.Screen name='Create' component={SignUp} options={{headerShown:false}}/>
+        <Stack.Screen name='Admins' component={Admin} options={{headerShown:false}}/>
+        <Stack.Screen name='Customers' component={Customer} options={{headerShown:false}}/>
+        {/* <Stack.Screen name='Customers' component={QRCodeDisplay} options={{headerShown:false}}/> */}
+        <Stack.Screen name='Cashiers' component={Cashier} options={{headerShown:false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
